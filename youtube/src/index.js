648b7bcbee64da4072bccfb18b43702573a0982e
Song jae hyeok <wakij6587@naver.com>
2021-02-05 11:56:41 +0900

    오타 수정

--- youtube/src/index.js
+++ youtube/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './app.jsx';
 import Youtube from './service/youtube';
 
-const youtube = new Youtube(process.env.REACT_APP_YOUYUBE_API);
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
 ReactDOM.render(
   <>
     <App youtube={youtube} />
